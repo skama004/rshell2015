715b93a5b80a3c11cf61a024034c6a02dc8d1344
Ignacy Chorazewicz <ichor001@hammer.cs.ucr.edu>
2015-04-27 15:16:35 -0700

    test files, STL copy
    + added STL copy function
    + added two test files for copying in test/
    + added cp header

--- src/cp.cpp
+++ src/cp.cpp
@@ -5,16 +5,39 @@
 #include <errno.h>
 
 #include <iostream>
+#include <fstream>
 
+#include "cp.h"
 
-int main(int argc, char** argv) {
+int main(int argc, const char** argv) {
     
-    if(argc != 3) {
+    if(argc != 3 && argc != 4) {
         std::cout << "Invalid number of arguments" << std::endl;
         return 1;
     }
 
+    const char* _src = argv[1];
+    const char* _dst = argv[2];
     
+    stdcopy(_src, _dst); 
 
     return 0;
 }
+
+
+int stdcopy(const char* srcpath, const char* dstpath) {
+    
+    std::ifstream srcfile(srcpath);
+    std::ofstream dstfile(dstpath);
+    char c;
+   
+    while(srcfile.get(c))
+        dstfile.put(c);
+
+    srcfile.close();
+
+    return 0;
+}
+
+
+

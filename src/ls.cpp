8e63de5f71ef9329d4ad5fdf462d6ccb7313f21f
Siddaardha Kamarsu <skama004@ucr.edu>
2015-04-29 15:18:35 -0700

    Updated functions

--- src/ls.cpp
+++ src/ls.cpp
@@ -5,19 +5,57 @@
 #include <dirent.h> //USES DIR, DIRENT, OPENDIR, ETC.
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+
 using namespace std;
 
-int blocks(char* directoryname. int flag)
+int blocks(char* directoryname, int flag)
 {
 	int blk = 0;
 	DIR *dirStream;
 	dirent* currentdirectory;
 
-	if (!dirStream = opendir(directoryname)))
+	if (!(dirStream = opendir(directoryname)))
 	{
 		perror("opendir"); // Error with opendir syscall
 	}
+	
+	while((currentdirectory = readdir(dirStream))) //Read directory used here
+	{
+		if(errno!=0)
+		{
+			perror("readdir");
+		}
+		struct stat x;
 
+		char path[1000];
+
+		strcpy(path, directoryname);
+		strcat(path, "/");
+		strcat(path, currentdirectory->d_name);
+		if (stat(path, &x) == -1)
+		{
+			perror("stat");
+		}
+
+		if (flag % 2 == 0)
+		{
+			blk += x.st_blocks;
+		}
+
+		else
+		{
+			if (currentdirectory->d_name[0] != '.')
+			{
+				blk += x.st_blocks;
+			}
+		}
+	}
+	if (closedir(dirStream) == -1)
+	{
+		perror("closedir");
+	}
+	return blk;
 }
 
 

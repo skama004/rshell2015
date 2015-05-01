dcf6d6750b51546d695bd221e442cf486777112e
Siddaardha Kamarsu <skama004@ucr.edu>
2015-04-30 18:47:33 -0700

    updated ls

--- src/ls.cpp
+++ src/ls.cpp
@@ -6,10 +6,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
-
+#include <algorithm> 
+#include <vector>
 using namespace std;
 
-int blocks(char* directoryname, int flag)
+int blocks(char* directoryname, int flag) //Acts as a helper for dir syscalls
 {
 	int blk = 0;
 	DIR *dirStream;
@@ -22,30 +23,30 @@ int blocks(char* directoryname, int flag)
 	
 	while((currentdirectory = readdir(dirStream))) //Read directory used here
 	{
-		if(errno!=0)
+		if(errno!=0) //Will return an error if anything but 0
 		{
 			perror("readdir");
 		}
-		struct stat x;
+		struct stat x; //Create a struct stat 
 
 		char path[1000];
 
 		strcpy(path, directoryname);
 		strcat(path, "/");
 		strcat(path, currentdirectory->d_name);
-		if (stat(path, &x) == -1)
+		if (stat(path, &x) == -1) //Stat returns -1 if error
 		{
 			perror("stat");
 		}
 
-		if (flag % 2 == 0)
+		if (flag % 2 == 0) //Mod by 2
 		{
 			blk += x.st_blocks;
 		}
 
 		else
 		{
-			if (currentdirectory->d_name[0] != '.')
+			if (currentdirectory->d_name[0] != '.') //Checks for . 
 			{
 				blk += x.st_blocks;
 			}
@@ -59,7 +60,7 @@ int blocks(char* directoryname, int flag)
 }
 
 
-void recursioncall(char *directoryname, int flag)
+void recursioncall(char *directoryname, int flag) //Function for -R
 {
 	DIR* dirStream;
 	dirent* currentdirectory;
@@ -80,6 +81,23 @@ void recursioncall(char *directoryname, int flag)
 	if(flag%3 == 0)
 	{
 		block = blocks(directoryname, flag);
+		cout << "Total: " << (block/2) << endl;
+	}
+
+	vector <char*> cstrings;
+
+	while ((currentdirectory = readdir(dirStream)))
+	{
+		cstrings.push_back(currentdirectory->d_name);//Copy contents of file into cstrings 
+	}
+	sort(cstrings.begin(), cstrings.end()); //Sort the cstrings "vector" to be in order
+
+	for (unsigned i = 0; i < cstrings.size(); i++)
+	{
+		if (errno!= 0)
+		{
+			perror("readdir"); //Error if not equal to 0 with errno
+		}
 	}
 }
 

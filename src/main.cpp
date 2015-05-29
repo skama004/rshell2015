1fd10106ab1d4a85ecbe67ce5a3ee40b131afceb
Siddaardha Kamarsu <skama004@ucr.edu>
2015-05-28 19:06:57 -0700

    Added cd functions. Error with rshell that needs to be debugged

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,88 @@
 #include <sys/wait.h>
 using namespace std;
 
-//char semicols = " ; ";
-//char ands = " && ";
-//char ors = " || ";
-//char spaces = " ";
+void display();
+
+void sigctrlC(int i)
+{
+	signal(SIGINT, SIG_IGN);
+}
 
 void executecmd(char **argv, int &status)
 {
-	int pid = fork();
+	string path = getenv("PATH");
+		if (path.empty())
+		{
+			perror("getenv");
+		}
+
+		vector<string> dirPath;
+		while(!(path.find(":") == string::npos))
+		{
+			dirPath.push_back(path.substr(0, path.find(":")));
+			path = path.substr(path.find(":")+1);
+		}
+		
+		int exec = 0;
+		for (unsigned i = 0; i < dirPath.size(); i++)
+		{
+			string temp = argv[0];
+			dirPath[i].append("/");
+			dirPath[i].append(temp);
+				if ((exec = (access(const_cast<char*> (dirPath[i].c_str()), X_OK))) == 0)
+				{
+					if (execv(const_cast<char*>(dirPath.at(i).c_str()), argv) == -1)
+					{
+						perror("execvp");
+						display();
+						//exit(0);
+					}
+				}
+		}
+
+		if (exec == -1)
+		{
+			perror("access");
+			exit(1);
+		}
+
+	 int pid = fork();
   
  	 if (pid == 0)
 	 {
-	 	 if (-1 == execvp(argv[0],argv))//Runs execvp to whatever arguments are passed through
+	 	 /*if (-1 == execvp(argv[0],argv))//Runs execvp to whatever arguments are passed through
 		 {
 			 status = -1;//Sets status to -1 to indicate 
 	       perror("Error w/ execvp");
 		 	// cout << "It works";
 		 }
 		 exit(1);//Return exit(1) if an error occured
+		// pipes1(status);*/
+		if(strcmp(argv[0], "cd") == 0)
+		{
+			if(status == 1)
+			{
+				cout << "cd working" << endl;
+				char *homedir = getenv("HOME");
+				if(chdir(homedir) == -1)
+				{
+					perror("chdir");
+				}
+			}
+			else
+			{
+				cout << "cd running " << endl;
+				if(chdir(argv[1]) == -1)
+				{
+					perror("chdir2");
+				}
+			}
+			//display();
+			//exit(0);
+		}
 	 }
 				
-	else if (pid == -1)//If fork returns an error
+	else if (pid == -1)//If for returns an error
 	{
 		perror("Error w/ fork");
 		exit(1);
@@ -47,6 +108,8 @@ void executecmd(char **argv, int &status)
 		}
 	}
 }
+
+
 
 void tokenize(char **argvinput, char **argvoutput, char *operation, int& size)
 {
@@ -66,175 +129,6 @@ void tokenize(char **argvinput, char **argvoutput, char *operation, int& size)
 
 	delete[] word; //Deallocates memory 
 		
-}
-
-void initializetok(char *charinput, char** argv)//Use this to initialize first token
-{
-	char *store = strtok(charinput, ""); //Tokenizes the input and stores it into store
-	int i = 0;
-	while (store != NULL) //Keeps going until the stored word is null
-	{
-		argv[i] = store; //Passes the stored word in argument array
-
-		store = strtok(NULL, ""); //Tokenizes null word again and stores yet again
-		i++;
-	}
-
-	argv[i] = 0;
-	delete [] store; //Deallocates memory
-}
-
-void stringtoken(string input)
-{
-	char *inputchar = new char[input.length() + 1]; //Creates a char pointer from string;
-	char *exitcmd = new char[5];
-	bool semicol = false;
-	bool andd = false;
-	bool orr = false; 
-	bool space = true;
-
-	char *semicols = new char[3];
-	char *ands = new char [3];
-	char *ors = new char [3];
-	char *spaces = new char[3];
-	//newly allocate memory to keep track of all the operators 
-	//also create bool values to determine tests whether they work or not
-	//
-	strcpy(inputchar, input.c_str());
-	strcpy(semicols, ";");
-	strcpy(ands,"&&");
-	strcpy(ors, "||");
-	strcpy(spaces, " \n");
-	strcpy(exitcmd, "exit");
-	//Strcpy will check if the operations passed through are in the string
-	
-	if (input.find(";") < input.size())
-	{
-		semicol = true; //If semicolon is found and is less than the size, then bool value will be set to true to determine it is there
-	}
-
-	if (input.find("&&") < input.size())
-	{
-		andd = true;//Same with and operator
-	}
-		
-	if (input.find("||") < input.size())
-	{
-		orr = true;//Same with or operator
-	}
-	//Sets the value to be true if operation is found in string
-	
-
-	char **argvsemicol = new char*[strlen(inputchar)];
-	char **argvands = new char*[strlen(inputchar)];
-	char **argvors = new char*[strlen(inputchar)];
-	char **argvspaces = new char*[strlen(inputchar)];
-	char **argvinp = new char*[strlen(inputchar)];
-	//Newly allocates memory again for operators passed through arguments
-	//strlen return the size of char array
-	//so newly allocated memory 
-	
-	initializetok(inputchar, argvinp);//Call initalizetok function to pass in parameters and set it up for further breakdown
-
-	int status = 0;//Acts as status of whether operators are found or not
-	int opstate = 0; //Acts as a check for operators
-
-	if (semicol || andd || orr || space) //Checks for all 4 operators
-	{
-		int size = 0;
-
-		tokenize(argvinp, argvsemicol, semicols, size);//Tokenizes the arguments for semicolons
-
-		for (int i = 0; i < size; i++)
-		{
-			if (andd || orr || space)
-			{
-				int size1 = 0; 
-				
-				tokenize(argvsemicol, argvands, ands, size1);//Tokenizes the arguments for ands
-				
-				for (int j = 0; j < size1; j++)
-				{
-					if (orr || space)
-					{
-						int size2 = 0;
-
-						tokenize(argvands, argvors, ors, size2);//Tokenizes the arguments for ors
-
-						for (int h = 0; h < size2; h++)
-						{
-							if (space)
-							{
-								int size3 = 0;
-
-								tokenize(argvors, argvspaces, spaces, size3);//Tokenizes the arguments for spaces
-
-								if (argvspaces[0] == NULL)
-								{
-									cerr << "Error!!"; //Outputs an error to determine if segault or not. Outputs an error, but still runs program. *Update in README*
-								}
-								else if (strcmp(argvspaces[0], exitcmd) == 0)
-								{
-									exit(0);//Exits without an error
-								}
-								else 
-								{
-									executecmd(argvspaces, status);//Calls commands for the arguments and runs execvp within that function
-								}
-							}
-							if (status == 0) //Will return 0 if it failed
-							{
-								h = size2;
-								opstate = -1;//Sets the operation status to -1 
-							}
-							else 
-							{
-								for (int k = 0; k < size2; k++)
-								{
-									argvors[k] = argvors[k+1]; //arguments of ors get copied over to 1+
-								}
-							}
-
-						}
-					}
-					
-					if (status == 0 || (ors&&opstate == -1))
-					{
-						for (int l = 0; l < size1; l++)
-						{
-							argvands[l] = argvands[l+1];
-							status = 200;
-						}
-					}
-					else
-					{
-						char *fals = new char[1];
-						strcpy(fals, "");
-						argvands[0] = fals;
-						delete [] fals;
-					}
-			}
-			
-		}
-		for (int m = 0; m < size; m++)
-		{
-			argvsemicol[m] = argvsemicol[m+1];
-		}
-
-	}
-	delete [] argvands;
-	delete [] argvors;
-	delete [] argvsemicol;
-	delete [] argvspaces;
-	delete [] argvinp;
-	delete [] semicols;
-	delete [] ands;
-	delete [] ors;
-	delete [] exitcmd;
-	delete [] inputchar;
-	delete [] spaces;
-	//Deletes all allocated memory to ensure no memory leak. ****IF MEMORY LEAK OCCURS GO THROUGH CODE AGAIN****
-	}
 }
 
 
@@ -397,13 +291,13 @@ void pipes1 (vector <string> cmdlist)
 		else //All error checking
 		{
 			int status = 0;
-			int restore = 0;
+			//int restore = 0;
 
 			if (waitpid(-1, &status, 0) == -1)
 			{
 				perror("waitpid");
 			}
-
+			int restore = 0;
 			if ((restore=dup(0)) == -1)
 			{
 				perror("dup");
@@ -429,8 +323,196 @@ void pipes1 (vector <string> cmdlist)
 	}
 }
 
+void initializetok(char *charinput, char** argv)//Use this to initialize first token
+{
+	char* store = strtok(charinput, ""); //Tokenizes the input and stores it into store
+	int i = 0;
+	while (store != NULL) //Keeps going until the stored word is null
+	{
+		argv[i] = store; //Passes the stored word in argument array
+
+		store = strtok(NULL, ""); //Tokenizes null word again and stores yet again
+		i++;
+	}
+	/*
+	int forktest = fork();
+	if (forktest == -1)
+	{
+		perror("fork");
+		exit(1);
+	}
+
+	else if (forktest == 0)
+	{
+		vector<string> v (&store, &store + 10);
+		pipes1(&store);
+	}
+	*/
+
+
+	argv[i] = 0;
+	delete [] store; //Deallocates memory
+}
+
+
+void stringtoken(string input)
+{
+	char *inputchar = new char[input.length() + 1]; //Creates a char pointer from string;
+	char *exitcmd = new char[5];
+	bool semicol = false;
+	bool andd = false;
+	bool orr = false; 
+	bool space = true;
+
+	char *semicols = new char[3];
+	char *ands = new char [3];
+	char *ors = new char [3];
+	char *spaces = new char[3];
+	//newly allocate memory to keep track of all the operators 
+	//also create bool values to determine tests whether they work or not
+	//
+	strcpy(inputchar, input.c_str());
+	strcpy(semicols, ";");
+	strcpy(ands,"&&");
+	strcpy(ors, "||");
+	strcpy(spaces, " \n");
+	strcpy(exitcmd, "exit");
+	//Strcpy will check if the operations passed through are in the string
+	
+	if (input.find(";") != string::npos)
+	{
+		semicol = true; //If semicolon is found and is less than the size, then bool value will be set to true to determine it is there
+	}
+
+	if (input.find("&&") != string::npos)
+	{
+		andd = true;//Same with and operator
+	}
+		
+	if (input.find("||") != string::npos)
+	{
+		orr = true;//Same with or operator
+	}
+	//Sets the value to be true if operation is found in string
+	
+
+	char **argvsemicol = new char*[strlen(inputchar)];
+	char **argvands = new char*[strlen(inputchar)];
+	char **argvors = new char*[strlen(inputchar)];
+	char **argvspaces = new char*[strlen(inputchar)];
+	char **argvinp = new char*[strlen(inputchar)];
+	//Newly allocates memory again for operators passed through arguments
+	//strlen return the size of char array
+	//so newly allocated memory 
+	
+	initializetok(inputchar, argvinp);//Call initalizetok function to pass in parameters and set it up for further breakdown
+
+	int status = 0;//Acts as status of whether operators are found or not
+	int opstate = 0; //Acts as a check for operators
+
+	if (semicol || andd || orr || space) //Checks for all 4 operators
+	{
+		int size = 0;
+
+		tokenize(argvinp, argvsemicol, semicols, size);//Tokenizes the arguments for semicolons
+
+		for (int i = 0; i < size; i++)
+		{
+			if (andd || orr || space)
+			{
+				int size1 = 0; 
+				
+				tokenize(argvsemicol, argvands, ands, size1);//Tokenizes the arguments for ands
+				
+				for (int j = 0; j < size1; j++)
+				{
+					if (orr || space)
+					{
+						int size2 = 0;
+
+						tokenize(argvands, argvors, ors, size2);//Tokenizes the arguments for ors
+
+						for (int h = 0; h < size2; h++)
+						{
+							if (space)
+							{
+								int size3 = 0;
+
+								tokenize(argvors, argvspaces, spaces, size3);//Tokenizes the arguments for spaces
+
+								if (argvspaces[0] == NULL)
+								{
+									cerr << "error with execvp" << endl; //Outputs an error to determine if segault or not. Outputs an error, but still runs program. *Update in README*
+								}
+								else if (strcmp(argvspaces[0], exitcmd) == 0)
+								{
+									exit(0);//Exits without an error
+								}
+								else 
+								{
+									executecmd(argvspaces, status);//Calls commands for the arguments and runs execvp within that function
+								}
+							}
+							if (status == 0) //Will return 0 if it failed
+							{
+								h = size2;
+								opstate = -1;//Sets the operation status to -1 
+							}
+							else 
+							{
+								for (int k = 0; k < size2; k++)
+								{
+									argvors[k] = argvors[k+1]; //arguments of ors get copied over to 1+
+								}
+							}
+
+						}
+					}
+					
+					if (status == 0 || (ors&&opstate == -1))
+					{
+						for (int l = 0; l < size1; l++)
+						{
+							argvands[l] = argvands[l+1];
+							status = 200;
+						}
+					}
+					else
+					{
+						char *fals = new char[1];
+						strcpy(fals, "");
+						argvands[0] = fals;
+				//		connectors(fals);
+						delete [] fals;
+					}
+			}
+			
+		}
+		for (int m = 0; m < size; m++)
+		{
+			argvsemicol[m] = argvsemicol[m+1];
+		}
+
+	}
+	delete [] argvands;
+	delete [] argvors;
+	delete [] argvsemicol;
+	delete [] argvspaces;
+	delete [] argvinp;
+	delete [] semicols;
+	delete [] ands;
+	delete [] ors;
+	delete [] exitcmd;
+	delete [] inputchar;
+	delete [] spaces;
+	//Deletes all allocated memory to ensure no memory leak. ****IF MEMORY LEAK OCCURS GO THROUGH CODE AGAIN****
+	}
+}
+
 void display()
 {
+	signal(SIGINT, sigctrlC);
+
 	string prompt; //Created a string for the prompt ($ and hopefully login)
 	char host[333];
 
@@ -474,13 +556,11 @@ void display()
 		{
 			userinp.resize(userinp.find("#"));//Resizes if find comment returns -1	
 		}
-		connectors(userinp); //Setup for I/O and pipes
+			connectors(userinp);
 
-		stringtoken(userinp);
-
+			stringtoken(userinp);
 	}
 }
-
 
 int main()
 {
